61fd1f8abb73e0e7f0b2c3457102c9446ef5b97c
Yotam Granada <yotam.granada@gmail.com>
2022-11-09 13:41:33 +0200

    fetch post comments

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,9 +1,28 @@
+import { useState, useContext, useEffect } from "react";
+
 const Post = ({ post }) => {
+  const [postComments, setPostComments] = useState([]);
+
+  useEffect(() => {
+    const fetchPostComments = async () => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`
+      );
+      const postComments = await res.json();
+      console.log("postComments", postComments);
+      setPostComments(postComments);
+    };
+    fetchPostComments();
+  }, []);
+
   return (
+    
     <div className="post-container">
       <h4 className="post-header">{post.title}</h4>
       <p className="post-body">{post.body}</p>
+      {console.log("postComments",postComments)}
     </div>
+    
   );
 };
 
--- src/components/userPosts/UserPosts.js
+++ src/components/userPosts/UserPosts.js
@@ -7,6 +7,7 @@ const UsersPosts = () => {
   const { userId } = useParams();
   let { user, SetUser } = useContext(BlogContext);
   const [userPosts, SetUserPosts] = useState([]);
+
   useEffect(() => {
     !user.id &&
       fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
@@ -22,6 +23,24 @@ const UsersPosts = () => {
       .then((data) => SetUserPosts(data));
   }, []);
 
+  // useEffect(()=>{
+  //   const fetchPostComments = async () => {
+  //     const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+  //     const postComments = await res.json();
+  //     console.log("postComments", postComments);
+  //     setPostComments(postComments);
+  //     fetchPostComments()
+  //   }
+  // },[postId])
+
+  // const fetchPostComments = async (postId) => {
+  //    fetch(
+  //     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => data);
+  // };
+
   console.log("user", user);
 
   return (
@@ -29,9 +48,12 @@ const UsersPosts = () => {
       {/* {console.log("userPosts",userPosts)} */}
       <h1 className="user-posts-header">{user.name} Posts</h1>
       <div className="posts-container">
-        {userPosts.map((post) => (
-          <Post post={post} key={post.id} />
-        ))}
+        {userPosts.map((post) => {
+          // fetchPostComments(post.id);
+          // setPostId(post.id)
+
+          return <Post post={post} key={post.id} />;
+        })}
       </div>
 
       {/* <h3>{userPosts[0].title}</h3> */}
